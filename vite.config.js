304e3373d922e3f49b302e259c5f4368c74b56a2
Admin_DIL_SRV <leon.dietsche@riverty.com>
2025-04-02 13:52:55 +0200

    add some

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 // vite.config.js
 export default {
-    root: 'src/',
-    publicDir: '../static',
-    base: '/mariastudio/',
-    server: {
-      host: '0.0.0.0',
-      port: 5173, // Ensure this port is not blocked by your firewall
-    },
-  };
-  
+  root: 'src/',
+  publicDir: '../static',
+  base: '/mariastudio/',
+  build: {
+    outDir: "dist"  // ✅ Fixed missing comma
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173, // Ensure this port is not blocked by your firewall
+  }
+};
