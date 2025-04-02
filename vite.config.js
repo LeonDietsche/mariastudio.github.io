1d1071c0fdc53e2c7d24b6fab237f7d9fc0a41d5
Leon Dietsche <43369899+LeonDietsche@users.noreply.github.com>
2025-04-02 13:01:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 export default {
     root: 'src/',
     publicDir: '../static',
-    base: '/mariastudio.github.io',
+    base: '/mariastudio',
     server: {
       host: '0.0.0.0',
       port: 5173, // Ensure this port is not blocked by your firewall
